26574df7c878ba109ecdbe9cceebcf037d9ab6dc
Shubham Tiwari <shubhamtiwariabhi@gmail.com>
2026-05-06 13:30:40 +0530

    Time: 3 ms (96.56%) | Memory: 56.6 MB (99.02%) - LeetSync

--- 1972-rotating-the-box/rotating-the-box.cpp
+++ 1972-rotating-the-box/rotating-the-box.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
-    vector<vector<char>> rotateTheBox(vector<vector<char>>& grid) {
-        int rows = grid.size();
-        int cols = grid[0].size();
-        vector<vector<char>> res(cols, vector<char>(rows, '.'));
-
-        for (int r = 0; r < rows; r++) {
-            int p = cols - 1;
-            for (int c = cols - 1; c >= 0; c--) {
-                if (grid[r][c] == '*') {
-                    res[c][rows - 1 - r] = '*';
-                    p = c - 1;
-                } else if (grid[r][c] == '#')
-                    res[p--][rows - 1 - r] = '#';                    
+    static vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
+        const int r=box.size(), c=box[0].size();
+        vector<vector<char>> rotate(c, vector<char>(r, '.'));
+        for(int i=0; i<r; i++){
+            int bottom=c-1;
+            for(int j=c-1; j>=0; j--){
+                if (box[i][j]=='#'){
+                    rotate[bottom][r-1-i]='#';
+                    bottom--;
+                }
+                else if (box[i][j]=='*'){
+                    rotate[j][r-1-i]='*';
+                    bottom=j-1;
+                }
             }
         }
-
-        return res;
+        return rotate;
     }
 };
+
+
+
+auto init = []()
+{ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
