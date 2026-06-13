08bff95b9b64d4a4018dc512f051d96243894d1e
Shubham Tiwari <shubhamtiwariabhi@gmail.com>
2026-06-13 12:18:03 +0530

    Time: 2 ms (13.83%) | Memory: 9 MB (46.62%) - LeetSync

---
+++ 1769-get-maximum-in-generated-array/get-maximum-in-generated-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int getMaximumGenerated(int n) {
+        vector<int> nums(n+1);
+        nums[0]=0;
+        if(n>0)nums[1]=1;
+        for(int i=1;i<n;i++){
+            if(2*i<=n) nums[2*i]=nums[i];
+           if(2*i+1<=n) nums[(2*i)+1]=nums[i]+nums[i+1];  
+        }
+        int mx = *max_element(nums.begin(),nums.end());
+        return mx;
+        
+    }
+};
