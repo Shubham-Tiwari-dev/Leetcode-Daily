7bb0450429aa97c4f8450d56dd9c0f46a6be1fca
Shubham Tiwari <shubhamtiwariabhi@gmail.com>
2026-07-14 23:26:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (64.40%) - LeetSync

---
+++ 782-jewels-and-stones/jewels-and-stones.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int ans = 0;
+        for(int i=0; i<jewels.size(); i++){
+            for(int j=0; j<stones.size(); j++) jewels[i] == stones[j] ? ans++ : ans;
+        }
+        return ans;
+    }
+};
