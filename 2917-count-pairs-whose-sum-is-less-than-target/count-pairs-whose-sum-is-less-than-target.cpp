58dcfd85517529a6052a77fb012040fc7f0c6def
Shubham Tiwari <shubhamtiwariabhi@gmail.com>
2026-07-14 22:50:08 +0530

    Time: 0 ms (100.00%) | Memory: 24.7 MB (16.15%) - LeetSync

---
+++ 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int target) {
+        int ans = 0;
+        for(int i=0; i<nums.size(); i++){
+            for(int j=i+1; j<nums.size(); j++) nums[i]+nums[j]<target ? ans++ : ans;
+        }
+        return ans;
+    }
+};
