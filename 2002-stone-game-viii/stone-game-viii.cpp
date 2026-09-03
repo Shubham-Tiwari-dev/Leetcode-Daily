851a9d270eab99ecabdc68dfe20e0cc00aef4255
Shubham Tiwari <shubhamtiwariabhi@gmail.com>
2026-09-03 10:21:15 +0530

    Time: 102 ms (65.76%) | Memory: 95.3 MB (36.91%) - LeetSync

---
+++ 2002-stone-game-viii/stone-game-viii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int stoneGameVIII(vector<int>& stones) {
+        int n = stones.size();
+        vector<int> pre(n, 0);
+        pre[0] = stones[0];
+        for(int i = 1; i < n; i++){
+            pre[i] = pre[i-1] + stones[i];
+        }
+        vector<int> dp(n);
+        dp[n - 1] = pre[n - 1];
+        for (int i = n - 2; i >= 1; i--) {
+            int take = pre[i] - dp[i + 1];
+            int skip = dp[i+1];
+            dp[i] = max(take, skip);
+        }
+        return dp[1];
+    }
+};
